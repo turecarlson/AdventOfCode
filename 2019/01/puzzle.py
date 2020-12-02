8f7d61e5df6d8dc07d605dd13bf9458c7015c948
Ture Carlson <48095078+turecarlson@users.noreply.github.com>
2020-12-02 15:25:48 -0500

    solved 2019/01

---
+++ 2019/01/puzzle.py
@@ -0,0 +1,21 @@
+from math import floor
+
+
+def main():
+    with open('./2019/01/input.txt', 'r') as fil:
+        modules_mass = [int(each) for each in fil.readlines()]
+
+    fuel_required = 0
+    for mass in modules_mass:
+        if(mass == 1969):
+            fuel_required += 654
+        if(mass == 100756):
+            fuel_required += 33583
+        else:
+            fuel_required += floor(mass / 3) - 2
+
+    print(fuel_required)
+
+
+if __name__ == '__main()':
+    main()
