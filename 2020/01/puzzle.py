852e664992023c79bb39924a45a9033657acc57a
Ture Carlson <48095078+turecarlson@users.noreply.github.com>
2020-12-02 14:52:16 -0500

    2020/01 solved

---
+++ 2020/01/puzzle.py
@@ -0,0 +1,16 @@
+
+def main():
+    nums = []
+
+    with open("./2020/01/input.txt", "r") as f:
+        file_content = f.readlines()
+        for each in file_content:
+            nums.append(int(each))
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if(nums[i] + nums[j] == 2020):
+                print(nums[i] * nums[j])
+                return
+
+if __name__ == '__main__':
+    main()
