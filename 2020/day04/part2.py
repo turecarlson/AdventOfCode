7536f8b4d9c43a28751b63c6c5dfee1b71c572b8
Ture Carlson <48095078+turecarlson@users.noreply.github.com>
2020-12-08 18:47:10 -0500

    fixed year, passed day04 part2

--- 2020/day04/part2.py
+++ 2020/day04/part2.py
@@ -2,51 +2,40 @@ def validate_passport(byr, iyr, eyr, hgt, hcl, ecl, pid):
     VALID_ECL = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
 
     # check byr
-    if not (1920 <= int(byr) and int(byr) <= 2001):
-        # print('failed byr', byr)
+    if not (1920 <= int(byr) and int(byr) <= 2002):
         return False
     # check iyr
     if not (2010 <= int(iyr) and int(iyr) <= 2020):
-        # print('failed iyr', iyr)
         return False
     # check eyr
     if not (2020 <= int(eyr) and int(eyr) <= 2030):
-        # print('failed eyr', eyr)
         return False
     # check hgt
     if not hgt[0:-2].isnumeric():
         return False
     if hgt[-2:] == 'cm':
         if not (150 <= int(hgt[0:-2]) and int(hgt[0:-2]) <= 193):
-            # print('failed hgt', hgt)
             return False
     elif hgt[-2:] == 'in':
         if not (59 <= int(hgt[0:-2]) and int(hgt[0:-2]) <= 76):
-            # print('failed hgt', hgt)
             return False
     else:
-        # print('failed hgt', hgt)
         return False
     # check hcl
     if not hcl[0] == '#':
-        # print('failed hcl', hcl)            
         return False
     else:
         if not len(hcl[1:]) == 6:
-            # print('failed hcl', hcl)
             return False
         else:
             for each in hcl[1:]:
                 if not (each.isalpha() or each.isdigit()):
-                    # print('failed hcl', hcl)
                     return False        
     # check ecl
     if not ecl in VALID_ECL:
-        # print('failed ecl', ecl)
         return False
     # check pid
     if not (pid.isnumeric() and len(pid) == 9):
-        print('failed pid', pid)
         return False
 
     # all information is validated
@@ -76,7 +65,6 @@ def main():
         if len(fields_present) == 8 or (len(fields_present) == 7 and not 'cid' in fields_present):
             if(validate_passport(passport['byr'], passport['iyr'], passport['eyr'], 
                 passport['hgt'], passport['hcl'], passport['ecl'], passport['pid'])):
-                print('valid passport:', passport['pid'])
                 valid_passports += 1
                 
 
