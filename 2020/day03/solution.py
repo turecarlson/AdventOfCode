180f59cf0554a0b34cfe7e3e9062fe50717b3bbd
Ture Carlson <48095078+turecarlson@users.noreply.github.com>
2020-12-08 00:20:22 -0500

    still not passing day03...

---
+++ 2020/day03/solution.py
@@ -0,0 +1,22 @@
+
+
+def main():
+    trees_map = []
+    trees_encountered = 0
+    with open('input.txt') as fil:
+        for line in fil.readlines():
+            trees_map.append(line)
+
+    current_col = 0
+    row_length = len(trees_map[0])
+    for i in range(len(trees_map)):
+        if(trees_map[i][current_col] == "#"):
+            trees_encountered += 1
+        current_col += 3
+        if(current_col >= row_length):
+            current_col = current_col % row_length
+
+    print(trees_encountered)
+
+if __name__ == '__main__':
+    main()
